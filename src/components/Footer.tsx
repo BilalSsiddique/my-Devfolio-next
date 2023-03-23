499b35eea549c10f948c04c7005dbdcfe8d7219d
BilalSsiddique <bsiddiqui@students.uit.edu>
2023-03-23 22:14:21 +0500

    updated footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,12 +6,12 @@ const Footer = () => {
     <footer className="h-64 glass-navbar pt-10 ">
       <div className="w-5/6 mx-auto">
         <SocialMediaIcons />
-        <div className="md:flex justify-center md:justify-between text-center">
+        <div className="md:flex  justify-center md:justify-between text-center">
           <p className="font-playfair font-semibold text-2xl text-yellow">
             BILAL SIDDIQUE
           </p>
-          <p className="flex gap-1 font-playfair text-md ">
-            <FaCopyright size={25} />
+          <p className="flex gap-1 justify-center mt-4 font-playfair text-md ">
+            <FaCopyright size={20} />
             <p className="text-yellow">{" "} 2022 BILAL. All Rights Reserved.</p>
           </p>
         </div>
